af7ce612aaa2eba47388b1b5aea377b6bdcdd9b2
Tiana <Tiana.marie013@gmail.com>
2020-12-03 19:07:21 -0700

    delete

--- app/public/wp-content/themes/underscores copy/template-parts/navigation.php
+++ app/public/wp-content/themes/underscores copy/template-parts/navigation.php
@@ -1,30 +1,21 @@
-<div class="menu-wrapper">
 
-  <nav id="site-navigation" class="main-navigation">
+    <div class="menu-wrapper">
 
-    <div class="nav-menu">
-      <div class="menu">
-        <button class="menu-button" id="open-button">
-          <span>
-            <div></div>
-            <div></div>
-            <div></div>
-          </span>
-          Open Menu
-        </button>
-      </div>
-      <!-- 0000 -->
+      <nav id="site-navigation" class="main-navigation">
 
-      <?php
-      wp_nav_menu(
-        array(
-          'theme_location' => 'menu-1',
-          'menu_id'        => 'primary-menu',
-        )
-      );
-      ?>
-      <button class="menu-close" aria-label="close menu">x</button>
-  </nav><!-- #site-navigation -->
-</div>
+      <div class="nav-menu">
 
-</div>
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location' => 'menu-1',
+            'menu_id'        => 'primary-menu',
+          )
+        );
+        ?>
+        <button class="menu-close" aria-label="close menu">x</button>
+      </nav><!-- #site-navigation -->
+    </div>
+
+  </div>
+</di
