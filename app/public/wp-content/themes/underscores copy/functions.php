3eb1b6cefc11558c55821813d3a18dee05180701
Tiana <Tiana.marie013@gmail.com>
2020-12-06 05:32:10 -0700

    trying a diffrent build method

--- app/public/wp-content/themes/underscores copy/functions.php
+++ app/public/wp-content/themes/underscores copy/functions.php
@@ -206,22 +206,13 @@ $args = array(
    }
    add_action( 'widgets_init', 'main_widgets_init');
 
-   add_action('acf/init', 'my_acf_init_block_types');
-   function my_acf_init_block_types() {
-   
 	   // Check function exists.
 	   if( function_exists('acf_register_block_type') ) {
+		add_action('acf/init', 'my_acf_init_block_types');
+	   }
    
-		   // register a testimonial block.
-		   acf_register_block_type(array(
-			   'name'              => 'testimonial',
-			   'title'             => __('Testimonial'),
-			   'description'       => __('A custom testimonial block.'),
-			   'render_template'   => 'template-parts/blocks/testimonial/testimonial.php',
-			   'category'          => 'formatting',
-			   'icon'              => 'admin-comments',
-			   'keywords'          => array( 'testimonial', 'quote' ),
-		   ));
+	
+		   
 	 
 
 
