51e08b21f9820fcfaa6996d098a030a74754da75
Tiana <Tiana.marie013@gmail.com>
2020-12-05 18:51:50 -0700

    function added for custom plugin build

--- app/public/wp-content/themes/underscores copy/functions.php
+++ app/public/wp-content/themes/underscores copy/functions.php
@@ -206,6 +206,22 @@ $args = array(
    }
    add_action( 'widgets_init', 'main_widgets_init');
 
- 
+   add_action('acf/init', 'my_acf_init_block_types');
+   function my_acf_init_block_types() {
+   
+	   // Check function exists.
+	   if( function_exists('acf_register_block_type') ) {
+   
+		   // register a testimonial block.
+		   acf_register_block_type(array(
+			   'name'              => 'testimonial',
+			   'title'             => __('Testimonial'),
+			   'description'       => __('A custom testimonial block.'),
+			   'render_template'   => 'template-parts/blocks/testimonial/testimonial.php',
+			   'category'          => 'formatting',
+			   'icon'              => 'admin-comments',
+			   'keywords'          => array( 'testimonial', 'quote' ),
+		   ));
+	 
 
 
